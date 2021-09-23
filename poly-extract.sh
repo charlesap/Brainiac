649a0e859ae0ccb14a800faed037578ffd1a5371
Charles Perkins <chuck@kuracali.com>
2021-09-22 20:53:43 -0700

    rename main.poly to brainiac.poly

--- poly-extract.sh
+++ poly-extract.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-awk -F$'\U00002551' '{for(i=1;i<=NF;i++) print $i > ("data" i)}' main.poly
+awk -F$'\U00002551' '{for(i=1;i<=NF;i++) print $i > ("data" i)}' brainiac.poly
 mv data1 brainiac.c
 mv data2 brainiac.py
 mv data3 Brainiac.Mod
