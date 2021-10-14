ad60504790a7a2b444293fcc3dde2e484613efef
Charles Perkins <chuck@kuracali.com>
2021-10-14 07:19:56 -0700

    put brainiac.py in its place

--- poly-extract.sh
+++ poly-extract.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-rm -f brainiac.c brainiac.py Brainiac.Mod brainiac.f90 Brainiac.java brainiac.go
+
 awk -F║ '{for(i=1;i<=NF;i++) print $i > ("data" i)}' brainiac.poly
 mv data1 brainiac.c
-mv data2 brainiac.py
+mv data2 src/brainiac/brainiac.py
 mv data3 Brainiac.Mod
 mv data4 brainiac.f90
 mv data5 Brainiac.java
